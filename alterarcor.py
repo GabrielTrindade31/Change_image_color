4908e09e5977f361536b70690d40ed8b18dde648
N305-3015645\Gabriel <gtrindade2001@gmail.com>
2023-11-28 09:07:11 -0300

    With the examples

--- alterarcor.py
+++ alterarcor.py
@@ -2,115 +2,112 @@ import tkinter as tk
 from tkinter import filedialog, Scale, Toplevel, StringVar, Radiobutton, IntVar, DoubleVar  # Add DoubleVar
 from PIL import Image, ImageTk
 
+# Global variables
+image = None
+modified_image = None
 
+# Function to open the image file
+def open_file():
+    global image
+    path = filedialog.askopenfilename()
+    image = Image.open(path)
 
-# Variáveis globais
-imagem = None
-imagem_modificada = None
+    # Display the original image
+    image_tk = ImageTk.PhotoImage(image)
+    image_label.config(image=image_tk)
+    image_label.image = image_tk
 
-# Função para abrir o arquivo de imagem
-def abrir_arquivo():
-    global imagem
-    caminho = filedialog.askopenfilename()
-    imagem = Image.open(caminho)
-
-    # Exibir a imagem original
-    imagem_tk = ImageTk.PhotoImage(imagem)
-    imagem_label.config(image=imagem_tk)
-    imagem_label.image = imagem_tk
-
-# Função para aplicar a substituição de cor
-# Função para identificar se a imagem é RGB ou RGBA
-def identificar_canais():
-    if imagem:
-        if imagem.mode == "RGBA":
+# Function to apply color substitution
+# Function to identify if the image is RGB or RGBA
+def identify_channels():
+    if image:
+        if image.mode == "RGBA":
             return True
-        elif imagem.mode == "RGB":
+        elif image.mode == "RGB":
             return False
 
-# Função para acrescentar valores ao canal A
-def abrir_janela_acrescentar_alpha():
-    janela_acrescentar_alpha = Toplevel(root)
-    janela_acrescentar_alpha.title("Acrescentar Valor ao Canal A")
+# Function to add values to channel A
+def open_add_alpha_window():
+    add_alpha_window = Toplevel(root)
+    add_alpha_window.title("Add Value to Channel A")
 
-    # Slider para adicionar valor ao canal A
-    slider_alpha = Scale(janela_acrescentar_alpha, from_=0.0, to=1.0, resolution=0.01, orient=tk.HORIZONTAL, length=200, label="Adicionar ao Canal A")
-    slider_alpha.set(acresc_alpha_var.get())
-    slider_alpha.pack()
+    # Slider to add value to channel A
+    alpha_slider = Scale(add_alpha_window, from_=0.0, to=1.0, resolution=0.01, orient=tk.HORIZONTAL, length=200, label="Add to Channel A")
+    alpha_slider.set(add_alpha_var.get())
+    alpha_slider.pack()
 
-    # Botão para confirmar e aplicar o valor
-    botao_confirmar_alpha = tk.Button(janela_acrescentar_alpha, text="Confirmar", command=lambda: salvar_e_acrescentar_alpha(slider_alpha.get(), janela_acrescentar_alpha))
-    botao_confirmar_alpha.pack()
+    # Button to confirm and apply the value
+    confirm_alpha_button = tk.Button(add_alpha_window, text="Confirm", command=lambda: save_and_add_alpha(alpha_slider.get(), add_alpha_window))
+    confirm_alpha_button.pack()
 
-# Função para salvar e acrescentar valor ao canal A
-def salvar_e_acrescentar_alpha(acresc_alpha, janela):
-    acresc_alpha_var.set(acresc_alpha)
-    janela.destroy()
-    aplicar_cor()  # Chamar a função para atualizar a imagem modificada após a alteração do canal A
+# Function to save and add value to channel A
+def save_and_add_alpha(add_alpha, window):
+    add_alpha_var.set(add_alpha)
+    window.destroy()
+    apply_color()  # Call the function to update the modified image after changing channel A
 
-# Criar variável para armazenar o valor de acrescentar ao canal A
-def aplicar_cor():
-    global imagem, imagem_modificada
+# Create a variable to store the value to add to channel A
+def apply_color():
+    global image, modified_image
 
-    def dentro_da_faixa(pixel, r_faixa, g_faixa, b_faixa):
+    def within_range(pixel, r_range, g_range, b_range):
         r, g, b, _ = pixel
-        return r_faixa[0] <= r <= r_faixa[1] and g_faixa[0] <= g <= g_faixa[1] and b_faixa[0] <= b <= b_faixa[1]
+        return r_range[0] <= r <= r_range[1] and g_range[0] <= g <= g_range[1] and b_range[0] <= b <= b_range[1]
 
-    if imagem:
+    if image:
         r_min_val, r_max_val = r_min.get(), r_max.get()
         g_min_val, g_max_val = g_min.get(), g_max.get()
         b_min_val, b_max_val = b_min.get(), b_max.get()
-        a_val = acresc_alpha_var.get()  # Valor do canal A
+        a_val = add_alpha_var.get()  # Value of channel A
 
-        # Função para converter um pixel em um novo pixel
-        def converter_pixel(pixel):
+        # Function to convert a pixel to a new pixel
+        def convert_pixel(pixel):
             nonlocal r_min_val, r_max_val, g_min_val, g_max_val, b_min_val, b_max_val, a_val
-            if dentro_da_faixa(pixel, (r_min_val, r_max_val), (g_min_val, g_max_val), (b_min_val, b_max_val)):
-                # Acrescentar os valores especificados nos canais R, G, B e A
+            if within_range(pixel, (r_min_val, r_max_val), (g_min_val, g_max_val), (b_min_val, b_max_val)):
+                # Add the specified values to the R, G, B, and A channels
                 r, g, b, a = pixel
-                r += acresc_r_var.get()
-                g += acresc_g_var.get()
-                b += acresc_b_var.get()
+                r += add_r_var.get()
+                g += add_g_var.get()
+                b += add_b_var.get()
                 a += a_val
 
-                # Limitar os valores para estar no intervalo de 0 a 255 e 0.0 a 1.0
+                # Limit the values to be in the range of 0 to 255 and 0.0 to 1.0
                 r = max(0, min(255, int(r)))
                 g = max(0, min(255, int(g)))
                 b = max(0, min(255, int(b)))
                 a = max(0, min(255, int(a * 255)))  # Scale float alpha to integer range (0-255)
 
-                # Verificar a escolha da ordem dos canais
-                if ordem_var.get() == "RGBA":
+                # Check the choice of channel order
+                if order_var.get() == "RGBA":
                     return (r, g, b, a)
-                elif ordem_var.get() == "RBGA":
+                elif order_var.get() == "RBGA":
                     return (r, b, g, a)
-                elif ordem_var.get() == "BGRA":
+                elif order_var.get() == "BGRA":
                     return (b, g, r, a)
-                elif ordem_var.get() == "BRGA":
+                elif order_var.get() == "BRGA":
                     return (b, r, g, a)
-                elif ordem_var.get() == "GRBA":
+                elif order_var.get() == "GRBA":
                     return (g, r, b, a)
-                elif ordem_var.get() == "GBRA":
+                elif order_var.get() == "GBRA":
                     return (g, b, r, a)
             return pixel
 
+        # Apply color substitution to the image
+        modified_image = image.convert("RGBA" if identify_channels() else "RGB")
+        modified_image.putdata(list(map(convert_pixel, image.getdata())))
 
-        # Aplicar a substituição de cor à imagem
-        imagem_modificada = imagem.convert("RGBA" if identificar_canais() else "RGB")
-        imagem_modificada.putdata(list(map(converter_pixel, imagem.getdata())))
+        # Display the modified image
+        image_tk = ImageTk.PhotoImage(modified_image)
+        image_label.config(image=image_tk)
+        image_label.image = image_tk
 
-        # Exibir a imagem modificada
-        imagem_tk = ImageTk.PhotoImage(imagem_modificada)
-        imagem_label.config(image=imagem_tk)
-        imagem_label.image = imagem_tk
-
-# Função para criar um frame de faixa de valores
+# Function to create a range frame
 def create_range_frame():
     range_frame = tk.Frame(root)
     range_frame.pack()
 
-    # Configuração para o canal R (vermelho)
-    r_label = tk.Label(range_frame, text="Canal R:")
+    # Configuration for the R channel (red)
+    r_label = tk.Label(range_frame, text="R Channel:")
     r_label.pack(side=tk.LEFT)
 
     r_min_scale = Scale(range_frame, from_=0, to=255, orient=tk.HORIZONTAL, length=200, label="Min")
@@ -119,8 +116,8 @@ def create_range_frame():
     r_max_scale = Scale(range_frame, from_=0, to=255, orient=tk.HORIZONTAL, length=200, label="Max")
     r_max_scale.pack(side=tk.LEFT)
 
-    # Configuração para o canal G (verde)
-    g_label = tk.Label(range_frame, text="Canal G:")
+    # Configuration for the G channel (green)
+    g_label = tk.Label(range_frame, text="G Channel:")
     g_label.pack(side=tk.LEFT)
 
     g_min_scale = Scale(range_frame, from_=0, to=255, orient=tk.HORIZONTAL, length=200, label="Min")
@@ -129,8 +126,8 @@ def create_range_frame():
     g_max_scale = Scale(range_frame, from_=0, to=255, orient=tk.HORIZONTAL, length=200, label="Max")
     g_max_scale.pack(side=tk.LEFT)
 
-    # Configuração para o canal B (azul)
-    b_label = tk.Label(range_frame, text="Canal B:")
+    # Configuration for the B channel (blue)
+    b_label = tk.Label(range_frame, text="B Channel:")
     b_label.pack(side=tk.LEFT)
 
     b_min_scale = Scale(range_frame, from_=0, to=255, orient=tk.HORIZONTAL, length=200, label="Min")
@@ -141,109 +138,112 @@ def create_range_frame():
 
     return r_min_scale, r_max_scale, g_min_scale, g_max_scale, b_min_scale, b_max_scale
 
-# Função para criar a janela de troca de ordem dos canais
-def abrir_janela_trocar_ordem():
-    janela_trocar = Toplevel(root)
-    janela_trocar.title("Trocar Ordem dos Canais")
+# Function to create the channel order change window
+def open_order_change_window():
+    order_change_window = Toplevel(root)
+    order_change_window.title("Change Channel Order")
 
-    opcoes = ["RGBA", "RBGA", "BGRA", "BRGA", "GRBA", "GBRA"]
+    options = ["RGBA", "RBGA", "BGRA", "BRGA", "GRBA", "GBRA"]
 
-    # Variável para armazenar a opção selecionada
-    escolha_var = StringVar(value=ordem_var.get())
+    # Variable to store the selected option
+    choice_var = StringVar(value=order_var.get())
 
-    # Criar botões de opção para as ordens dos canais
-    for opcao in opcoes:
-        botao_opcao = Radiobutton(janela_trocar, text=opcao, variable=escolha_var, value=opcao)
-        botao_opcao.pack()
+    # Create option buttons for channel orders
+    for option in options:
+        option_button = Radiobutton(order_change_window, text=option, variable=choice_var, value=option)
+        option_button.pack()
 
-    # Botão para confirmar a escolha
-    botao_confirmar = tk.Button(janela_trocar, text="Confirmar", command=lambda: salvar_e_trocar_ordem(escolha_var.get(), janela_trocar))
-    botao_confirmar.pack()
+    # Button to confirm the choice
+    confirm_button = tk.Button(order_change_window, text="Confirm", command=lambda
 
-# Função para salvar e trocar a ordem dos canais
-def salvar_e_trocar_ordem(ordem, janela):
-    ordem_var.set(ordem)
-    janela.destroy()
+: save_and_change_order(choice_var.get(), order_change_window))
+    confirm_button.pack()
 
-# Função para criar a janela de acrescentar valores aos canais
-def abrir_janela_acrescentar():
-    janela_acrescentar = Toplevel(root)
-    janela_acrescentar.title("Acrescentar Valores aos Canais")
+# Function to save and change the channel order
+def save_and_change_order(order, window):
+    order_var.set(order)
+    window.destroy()
 
-    # Sliders para adicionar valores aos canais R, G, B
-    slider_r = Scale(janela_acrescentar, from_=-255, to=255, orient=tk.HORIZONTAL, length=200, label="Adicionar ao Canal R")
-    slider_r.set(acresc_r_var.get())
+# Function to create the add values to channels window
+def open_add_values_window():
+    add_values_window = Toplevel(root)
+    add_values_window.title("Add Values to Channels")
+
+    # Sliders to add values to R, G, B channels
+    slider_r = Scale(add_values_window, from_=-255, to=255, orient=tk.HORIZONTAL, length=200, label="Add to R Channel")
+    slider_r.set(add_r_var.get())
     slider_r.pack()
 
-    slider_g = Scale(janela_acrescentar, from_=-255, to=255, orient=tk.HORIZONTAL, length=200, label="Adicionar ao Canal G")
-    slider_g.set(acresc_g_var.get())
+    slider_g = Scale(add_values_window, from_=-255, to=255, orient=tk.HORIZONTAL, length=200, label="Add to G Channel")
+    slider_g.set(add_g_var.get())
     slider_g.pack()
 
-    slider_b = Scale(janela_acrescentar, from_=-255, to=255, orient=tk.HORIZONTAL, length=200, label="Adicionar ao Canal B")
-    slider_b.set(acresc_b_var.get())
+    slider_b = Scale(add_values_window, from_=-255, to=255, orient=tk.HORIZONTAL, length=200, label="Add to B Channel")
+    slider_b.set(add_b_var.get())
     slider_b.pack()
 
-    # Botão para confirmar e aplicar os valores
-    botao_confirmar = tk.Button(janela_acrescentar, text="Confirmar", command=lambda: salvar_e_acrescentar_valores(slider_r.get(), slider_g.get(), slider_b.get(), janela_acrescentar))
-    botao_confirmar.pack()
+    # Button to confirm and apply the values
+    confirm_button = tk.Button(add_values_window, text="Confirm", command=lambda: save_and_add_values(slider_r.get(), slider_g.get(), slider_b.get(), add_values_window))
+    confirm_button.pack()
 
-# Função para salvar e acrescentar valores aos canais R, G, B
-def salvar_e_acrescentar_valores(acresc_r, acresc_g, acresc_b, janela):
-    acresc_r_var.set(acresc_r)
-    acresc_g_var.set(acresc_g)
-    acresc_b_var.set(acresc_b)
-    janela.destroy()
+# Function to save and add values to R, G, B channels
+def save_and_add_values(add_r, add_g, add_b, window):
+    add_r_var.set(add_r)
+    add_g_var.set(add_g)
+    add_b_var.set(add_b)
+    window.destroy()
 
-# Função para salvar a imagem modificada
-def salvar_imagem():
-    global imagem_modificada
-    if imagem_modificada:
-        caminho = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("Arquivos PNG", "*.png")])
-        if caminho:
-            imagem_modificada.save(caminho)
+# Function to save the modified image
+def save_image():
+    global modified_image
+    if modified_image:
+        path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG Files", "*.png")])
+        if path:
+            modified_image.save(path)
 
-# Criar a janela principal
+# Create the main window
 root = tk.Tk()
-root.title("Substituição de cor")
+root.title("Color Substitution")
 
-# Criar um frame para os widgets
+# Create a frame for widgets
 frame = tk.Frame(root)
 frame.pack()
 
-acresc_alpha_var = DoubleVar(value=1.0)
-botao_acrescentar_alpha = tk.Button(frame, text="Acrescentar ao Canal A", command=abrir_janela_acrescentar_alpha)
-botao_acrescentar_alpha.pack()
-# Criar um botão para abrir o arquivo de imagem
-abrir_arquivo_button = tk.Button(frame, text="Abrir arquivo", command=abrir_arquivo)
-abrir_arquivo_button.pack()
+add_alpha_var = DoubleVar(value=1.0)
+add_alpha_button = tk.Button(frame, text="Add to Channel A", command=open_add_alpha_window)
+add_alpha_button.pack()
 
-# Criar um rótulo para exibir a imagem modificada
-imagem_label = tk.Label(root)
-imagem_label.pack()
+# Create a button to open the image file
+open_file_button = tk.Button(frame, text="Open file", command=open_file)
+open_file_button.pack()
 
-# Criar um botão para aplicar a substituição de cor
-aplicar_button = tk.Button(frame, text="Aplicar cor", command=aplicar_cor)
-aplicar_button.pack()
+# Create a label to display the modified image
+image_label = tk.Label(root)
+image_label.pack()
 
-# Criar um frame de faixa de valores
+# Create a button to apply color substitution
+apply_button = tk.Button(frame, text="Apply color", command=apply_color)
+apply_button.pack()
+
+# Create a range frame
 r_min, r_max, g_min, g_max, b_min, b_max = create_range_frame()
 
-# Criar variáveis para armazenar a escolha de ordem e os valores de acrescentar
-ordem_var = StringVar(value="RGBA")
-acresc_r_var = IntVar(value=0)
-acresc_g_var = IntVar(value=0)
-acresc_b_var = IntVar(value=0)
+# Create variables to store the choice of order and values to add
+order_var = StringVar(value="RGBA")
+add_r_var = IntVar(value=0)
+add_g_var = IntVar(value=0)
+add_b_var = IntVar(value=0)
 
-# Criar botões "Trocar" e "Acrescentar"
-botao_trocar = tk.Button(frame, text="Trocar", command=abrir_janela_trocar_ordem)
-botao_trocar.pack()
+# Create "Change" and "Add" buttons
+change_button = tk.Button(frame, text="Change", command=open_order_change_window)
+change_button.pack()
 
-botao_acrescentar = tk.Button(frame, text="Acrescentar", command=abrir_janela_acrescentar)
-botao_acrescentar.pack()
+add_button = tk.Button(frame, text="Add", command=open_add_values_window)
+add_button.pack()
 
-# Botão para salvar a imagem
-botao_salvar = tk.Button(frame, text="Salvar Imagem", command=salvar_imagem)
-botao_salvar.pack()
+# Button to save the image
+save_button = tk.Button(frame, text="Save Image", command=save_image)
+save_button.pack()
 
-# Iniciar o loop principal
+# Start the main loop
 root.mainloop()
